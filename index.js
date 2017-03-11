6e0e05a471617d24727cfb55938f0387af1ab0e4
Ryan Kramer <rkramer613@gmail.com>
2017-03-11 13:55:46 -0700

    Ran with nodemon and postman

--- index.js
+++ index.js
@@ -5,11 +5,23 @@ var app = express();
 
 // always parse http body as json
 // assigns result to req.body
+// 1.
 app.use(bodyParser.json());
+
+// 2.
+// must be a http POST method
+// if path === '/books'
+// then run the callback function
+app.post('/books', function(req, res, next) {
+		var incoming_body = req.body;
+		console.log(incoming_body);
+		res.json(incoming_body);
+});
+
 
 
 
 var port = 3000;
-app.listen(port function(){
+app.listen(port, function(){
     console.log('listening on port ' + port);
 });

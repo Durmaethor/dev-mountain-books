a2bb96e7f9bf06347b7d55479440e0aeab64ebfd
Ryan Kramer <rkramer613@gmail.com>
2017-03-11 13:34:57 -0700

    Ported and parsed, listening

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 var express    = require('express');
 var bodyParser = require('body-parser');
 
+var app = express();
+
+// always parse http body as json
+// assigns result to req.body
+app.use(bodyParser.json());
+
+
+
+var port = 3000;
+app.listen(port function(){
+    console.log('listening on port ' + port);
+});
